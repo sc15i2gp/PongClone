2faa897e609d56f223826bf27f02e6113acbd6aa
Isaac <sc15i2gp@leeds.ac.uk>
2018-05-20 13:24:27 +0100

    Extracted graphical methods from main.

--- src/main.cpp
+++ src/main.cpp
@@ -22,29 +22,25 @@
 const char vShader[] =
 "#version 410 core\n"
 "layout (location = 0) in vec3 position;\n"
+"layout (location = 1) in vec3 inColour;\n"
+"out vec3 colour;\n"
 "void main()\n"
 "{\n"
-"gl_Position = vec4(position.x, position.y, position.z, 1.0f);\n"
+"gl_Position = vec4(position, 1.0f);\n"
+"colour = inColour;\n"
 "}\0";
 
 const char fShader[] =
 "#version 410 core\n"
+"in vec3 colour;\n"
 "out vec4 fragColour;\n"
 "void main()\n"
 "{\n"
-"fragColour = vec4(1.0f, 0.0f, 0.0f, 1.0f);\n"
+"fragColour = vec4(colour, 1.0f);\n"
 "}\0";
 
-
-int main(int argc, char** argv)
+GLuint loadShader()
 {
-  SFML_Window window(960, 540, "Pong");
-  byte* windowBuffer = new byte[sizeof(sf::Window)];
-  window.init(windowBuffer);
-  LibInit::initGLEW();
-  float black[] = {0.0f, 0.0f, 0.0f};
-  window.setClearColour(black);
-
   const GLchar* vShaderSource = (const GLchar*)vShader;
   const GLchar* fShaderSource = (const GLchar*)fShader;
   GLuint vShaderID = glCreateShader(GL_VERTEX_SHADER);
@@ -56,7 +52,7 @@ int main(int argc, char** argv)
   if(!success)
   {
     glGetShaderInfoLog(vShaderID, 512, NULL, infoLog);
-    printf("Vertex shader compile failed: \n%s\n", infoLog);
+    printf("Vertex shader compilation failed: \n%s\n", infoLog);
     return -1;
   }
 
@@ -67,9 +63,8 @@ int main(int argc, char** argv)
   glGetShaderiv(fShaderID, GL_COMPILE_STATUS, &success);
   if(!success)
   {
-    GLsizei length = 1000000;
-    glGetShaderInfoLog(fShaderID, 512, &length, infoLog);
-    printf("Fragment shader compile failed: %d\n%s\n", length, infoLog);
+    glGetShaderInfoLog(fShaderID, 512, NULL, infoLog);
+    printf("Fragment shader compilation failed: \n%s\n", infoLog);
     return -1;
   }
   GLuint shader = glCreateProgram();
@@ -85,37 +80,57 @@ int main(int argc, char** argv)
   }
   glDeleteShader(vShaderID);
   glDeleteShader(fShaderID);
+  return shader;
+}
 
-  GLfloat positions[] =
-  {
-    -0.5f, 0.5f, 0.0f,
-     0.5f, 0.5f, 0.0f,
-     0.5f, -0.5f, 0.0f,
-    -0.5f, -0.5f, 0.0f
-  };
-  /*
-  GLfloat colours[] =
-  {
-    0.367f, 0.281f, 0.102f,
-    0.367f, 0.281f, 0.102f,
-    0.367f, 0.281f, 0.102f,
-    0.367f, 0.281f, 0.102f
-  };*/
-  GLuint indices[] = {0, 1, 2, 0, 2, 3};
-  GLuint VBO, VAO, EBO;
-  glGenVertexArrays(1, &VAO);
-  glGenBuffers(1, &VBO);
-  glGenBuffers(1, &EBO);
-  glBindVertexArray(VAO);
-  glBindBuffer(GL_ARRAY_BUFFER, VBO);
-  glBufferData(GL_ARRAY_BUFFER, sizeof(positions), positions, GL_STATIC_DRAW);
-  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(GLfloat), (GLvoid*)0);
-  glEnableVertexAttribArray(0);
-  glBindBuffer(GL_ARRAY_BUFFER, 0);
-  glBindVertexArray(0);
+GLuint loadRect()
+{
 
+    GLfloat positions[] =
+    {
+      -0.5f, 0.5f, 0.0f,
+       0.5f, 0.5f, 0.0f,
+       0.5f, -0.5f, 0.0f,
+      -0.5f, -0.5f, 0.0f
+    };
+
+    GLfloat colours[] =
+    {
+      0.367f, 0.281f, 0.102f,
+      0.367f, 0.281f, 0.102f,
+      0.367f, 0.281f, 0.102f,
+      0.367f, 0.281f, 0.102f
+    };
+    GLuint indices[] = {0, 1, 2, 0, 2, 3};
+    GLuint VBO[2], VAO, EBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(2, VBO);
+    glGenBuffers(1, &EBO);
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO[0]);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(positions), positions, GL_STATIC_DRAW);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(GLfloat), (GLvoid*)0);
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO[1]);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(colours), colours, GL_STATIC_DRAW);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 3*sizeof(GLfloat), (GLvoid*)0);
+    glEnableVertexAttribArray(1);
+    return VAO;
+}
+
+int main(int argc, char** argv)
+{
+  SFML_Window window(960, 540, "Pong");
+  byte* windowBuffer = new byte[sizeof(sf::Window)];
+  window.init(windowBuffer);
+  LibInit::initGLEW();
+  float black[] = {0.0f, 0.0f, 0.0f};
+  window.setClearColour(black);
+
+  GLuint shader = loadShader();
+  GLuint rect = loadRect();
   sf::Event e;
   bool running = true;
 
@@ -136,7 +151,7 @@ int main(int argc, char** argv)
     {
       window.clear();
       glUseProgram(shader);
-      glBindVertexArray(VAO);
+      glBindVertexArray(rect);
       glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
       window.update();
     }
